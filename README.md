5597abe74e33b1bcacd9f48401a04dbe27e32cfd
Pascal Allau <pascal.allau@etu.toulouse-inp.fr>
2022-05-19 17:58:31 +0200

    adding dist

--- README.md
+++
@@ -1,5 +0,0 @@
-# Base de code du projet P6 - Parcours Front-end
-
-## Démarrer le projet
-
-npm install.
--- scripts/pages/photographer.js
+++
@@ -1,8 +0,0 @@
-import '../../css/style.scss';
-import { displayModal, closeModal } from '../utils/contactForm';
-
-document
-  .querySelector('.photograph-header .contact_button')
-  .addEventListener('click', displayModal);
-
-document.querySelector('.modal img').addEventListener('click', closeModal);
