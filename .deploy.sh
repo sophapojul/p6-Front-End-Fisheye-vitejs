2a444a8d636de15594e7ab011fae111a0d25a98f
Pascal Allau <pascal.allau@etu.toulouse-inp.fr>
2022-06-03 16:23:55 +0200

    update deploy

--- .deploy.sh
+++ .deploy.sh
@@ -22,6 +22,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/sophapojul/p6-vitejs.git main:gh-pages
+git push -f git@github.com:sophapojul/p6-vitejs.git main:gh-pages
 
 cd -
